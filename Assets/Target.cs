217ffd3c1f2bd16775d83693b64a6822e31f3452
frantute <102159238+frantute@users.noreply.github.com>
2022-09-05 09:28:48 -0300

    Initial commit

---
+++ Assets/Target.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Transform targetTR;
+    public float speed;
+
+    void Update()
+    {
+        Vector3 currentEulerAngles = transform.eulerAngles;
+        transform.LookAt(targetTR);
+        transform.eulerAngles = new Vector3(currentEulerAngles.x, transform.eulerAngles.y, currentEulerAngles.z);
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetTR.position, step);
+    }
+
+}
